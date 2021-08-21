2b041e816581932d9eaac82490a0c21f6adfd9e9
Nabil Ahmed <nabil.ahmednsu@gmail.com>
2021-08-21 20:29:05 +0600

    ready for heroku

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,24 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    serverStatus: "Running",
-  });
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/upload", uploadRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({
+      serverStatus: "Running",
+    });
+  });
+}
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
